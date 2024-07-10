8b1b82648b859b43adc3ce0f17d00ded53bf4109
Your Name <jtkimmn13@gmail.com>
2024-07-09 22:50:56 -0500

    Secure the table booking API

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics, viewsets, permissions
 from django.contrib.auth.models import User  # Import User model
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer, UserSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def index(request):
@@ -11,10 +12,12 @@ def index(request):
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated] 
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated] 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -24,3 +27,5 @@ class UserViewSet(viewsets.ModelViewSet):
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated] 
+
